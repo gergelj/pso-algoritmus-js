be7a98b18f254e4a6882da4d41ec9c7242acf809
Gergo <49314980+gregVader@users.noreply.github.com>
2020-09-04 17:06:36 +0200

    js file path bug fix

--- js/app/pso-algorithm-worker.js
+++ js/app/pso-algorithm-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('message', function(e){
-    importScripts('js/app/pso-algorithm.js', 'js/lodash.min.js', 'js/seedrandom.min.js');
+    importScripts('pso-algorithm.js', '../lodash.min.js', '../seedrandom.min.js');
     let pso = new PsoAlgorithm(parseFloat(e.data.maxIteration), parseFloat(e.data.populationSize), new FunctionConfig(e.data.functionName));
     pso.start();
     self.postMessage(pso.swarms);
